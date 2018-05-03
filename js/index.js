b1d82eb7e005e54cbde8673b4b053bf5b10a2943
usernamefengfen0711 <fengfen0101@163.com>
2018-05-03 10:26:07 +0800

    firstSubmit

---
+++ js/index.js
@@ -0,0 +1,121 @@
+		(function ($) {
+		    $.getUrlParam = function (name) {
+		        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+		        var r = window.location.search.substr(1).match(reg);
+		        if (r != null) return unescape(r[2]); return null;
+		    }
+		})(jQuery);
+		function getData(){
+			var str={
+				"brokerId":$.getUrlParam('brokerId'),
+				"siId":$.getUrlParam('siId')
+			}
+			var data=JSON.stringify(str)
+			console.log(data)
+			$.ajax({
+				url:URL1+"query_workroom_info_jsons.tml",
+				type:"POST",
+				data:data,
+				success:function(res){
+					console.log(res)
+					$('.img').attr("src",res.output.spPic)
+					$('.name').html(res.output.spName)
+					$('.introduce').html(res.output.spSign)
+					$('.presonal').attr('src',res.output.stdLogo)
+					$('.phone').attr('href',"tel:"+res.output.phoneNo)
+				},
+				error:function(){
+					console.log("回显错误")
+				}
+			})
+		}
+		getData()
+		
+		getList()
+		
+		function getProlist(code){
+			console.log(code)
+			var tagCode=code
+			if(code!=undefined&&code!="TAG000001"){
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"isAll": 1,
+	  				"pageIndex": 1,
+	  				"pageSize": 100,
+	  				"tagCode":code
+				}
+			}else{
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"isAll": 1,
+	  				"pageIndex": 1,
+	  				"pageSize": 100
+				}
+				
+			}
+			
+			var data=JSON.stringify(str)
+			console.log(data)
+			$.ajax({
+				url:URL1+"query_std_prod_list_jsons.tml",
+				type:"POST",
+				data:data,
+				success:function(res){
+					console.log(res)
+					if(res.output!=undefined){
+						var list=res.output,html="";
+						//console.log(list[0].attachList.attachUrl)
+						for(var i=0,j=list.length;i<j;i++){
+							html+='<li prodSaleCode="'+list[i].prodCode+'" prodCode="'+list[i].prodCodeList[0].prodCode+'" class="item"><img class="item_img" src="'+list[i].attachList[0].attachUrl+'" />'
+							html+='<p class="item_title">'+list[i].prodName+'</p><div class="bottom">'
+							if(list[i].prodRecmd!=undefined){
+								html+='<p>'+list[i].prodRecmd+'</p><span class="money">'+list[i].minPrice+'元起</span></div></li>'
+							}else{
+								html+='<p></p><span class="money">'+list[i].minPrice+'元起</span></div></li>'
+							}
+				
+						}
+						$('.empty').css("display","none")
+						$('.list').css("display","block")
+						$('.list').html(html)
+						listOpation()
+					}else{
+						$('.empty').css("display","block")
+						$('.list').css("display","none")
+						nextPage()
+					}
+					
+				},
+				error:function(){
+					console.log("回显错误")
+				}
+			})
+		}
+		getProlist()
+		
+		//下一页
+//		function nextPage(){
+//			$(".btn_next").on('click',function(){
+//				window.location.href="manage.html?siId="+$.getUrlParam('siId')
+//			})	
+//		}
+		
+		//详情
+		function listOpation(){
+			$(".item").on('click',function(){
+				if(!is_weixin()){
+					var xiang="刘http://h5.qtoubao.cn/details/index.html?prodSaleCode="+$(this).attr("prodSaleCode")+"&prodCode="+$(this).attr("prodCode")
+					Tiny.execute("opendetail('prodSaleCode="+xiang+"')")
+				}else{
+					window.location.href="http://h5.qtoubao.cn/details/index.html?prodSaleCode="+$(this).attr("prodSaleCode")+"&prodCode="+$(this).attr("prodCode")
+				}
+			})
+			function is_weixin(){
+            	var ua = navigator.userAgent.toLowerCase();
+            	if(ua.match(/MicroMessenger/i)=="micromessenger") {
+               		return true;
+             	} else {
+                	return false;
+            	}
+        	}
+		}
---
+++ js/manage.js
@@ -0,0 +1,161 @@
+	
+	(function ($) {
+	    $.getUrlParam = function (name) {
+	        var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	        var r = window.location.search.substr(1).match(reg);
+	        if (r != null) return unescape(r[2]); return null;
+	    }
+	})(jQuery);
+	//获取分类
+	getList()
+	
+	//获取列表
+	function getProlist(code,data){
+			console.log(code)
+			var tagCode=code
+			if(code!=undefined&&code!="TAG000001"){
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"isAll": 0,
+	  				"pageIndex": 1,
+	  				"pageSize": 100,
+	  				"tagCode":code
+				}
+			}else if(data!=undefined){
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"isAll": 0,
+	  				"pageIndex": 1,
+	  				"pageSize": 100,
+	  				"prodName":data
+				}
+			}else{
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"isAll": 0,
+	  				"pageIndex": 1,
+	  				"pageSize": 100
+				}
+			}
+			
+			var data=JSON.stringify(str)
+			console.log(data)
+			$.ajax({
+				url:URL1+"query_std_prod_list_jsons.tml",
+				type:"POST",
+				data:data,
+				success:function(res){
+					console.log(res)
+					$('#search_input').blur()
+					if(res.output!=undefined){
+						var list=res.output,html="",arrList=[];
+						for(var i=0,j=list.length;i<j;i++){
+							var str="",tageList=list[i].tagList;
+							console.log(tageList)
+							for(var m=0,n=tageList.length;m<n;m++){
+								if(str==""){
+									str+=tageList[m].tagCode
+								}else{
+									str+=","+tageList[m].tagCode
+								}
+							}
+							arrList.push(str)
+							html+='<li tagCode="'+arrList[i]+'"  class="item" cmpCode="'+list[i].cmpCode+'" invalidTime="'+list[i].invalidTime+'" prodCode="'+list[i].prodCode+'" prodName="'+list[i].prodName+'"  prodDesc="'+list[i].prodDesc+'" code="1">'
+							html+='<div class="left"><img class="check" src="img/no.png"/></div>'
+							html+='<div class="center"><img class="item_img" src="'+list[i].attachList[0].attachUrl+'"/><div class="item_title">'
+							if(list[i].prodRecmd!=undefined){
+								html+='<p>'+list[i].prodName+'</p><span class="money">'+list[i].minPrice+'元起</span><div class="bottom"><p>'+list[i].prodRecmd+'</p>'
+							}else{
+								html+='<p>'+list[i].prodName+'</p><span class="money">'+list[i].minPrice+'元起</span><div class="bottom"><p></p>'	
+							}
+							html+='</div></div></li>'
+						}
+						$('.list').css("display","block")
+						$('.empty').css("display","none")
+						$('.list').html(html)
+						handleClick()
+						haha()
+					}else{
+						$('.list').css("display","none")
+						$('.empty').css("display","block")
+					}
+					
+				},
+				error:function(){
+					console.log("回显错误")
+				}
+			})
+		}
+		getProlist()
+	
+	var arr=[]
+	function haha(){
+		$('.btn').on('click',function(){
+			
+			var list=$(".item")
+			for(var i=0,j=list.length;i<j;i++){
+				if($(".item").eq(i).attr('code')==2){
+					var str={
+						'cmpCode':$(".item").eq(i).attr('cmpCode'),
+						"invalidTime":$(".item").eq(i).attr('invalidTime'),
+						"prdCode":$(".item").eq(i).attr('prodCode'),
+						"prdDesc":$(".item").eq(i).attr('prodDesc'),
+						"prdName":$(".item").eq(i).attr('prodName'),
+						"tagCode":$(".item").eq(i).attr('tagCode')
+					}
+					arr.push(str)
+				}
+			}
+			if($.getUrlParam('first')!=undefined){
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"prdList": arr,
+	  				"isCreate": "Y"
+				}
+			}else{
+				var str={
+					"siId":$.getUrlParam('siId'),
+					"prdList": arr,
+	  				"isCreate": "N"
+				}
+			}
+				
+				var data=JSON.stringify(str)
+				console.log(data)
+				$.ajax({
+					url:URL1+"add_std_prod_info_jsons.tml",
+					type:"POST",
+					data:data,
+					success:function(res){
+						console.log(res)
+						arr=[]
+						if(res.code=="SYS_S_000"){
+							if($.getUrlParam('first')!=undefined){
+								var siId=$.getUrlParam('siId');
+								Tiny.execute('openindex('+siId+')')
+								//window.location.href="index.html?siId="+$.getUrlParam('siId')
+							}else{
+								getProlist()
+								//mui.alert("添加成功")
+							}
+						}
+					},
+					error:function(){
+						//mui.alert("添加失败，请重新选择")
+					}
+				})
+			
+			
+		})
+	}
+	function handleClick(){
+		$('.item').on("click",function(){
+			if($(this).attr("code")==1){
+				$(this).find(".check").attr("src","img/yes.png")
+				$(this).attr("code","2")
+			}else{
+				$(this).find(".check").attr("src","img/no.png")
+				$(this).attr("code","1")
+			}
+		})
+	}
